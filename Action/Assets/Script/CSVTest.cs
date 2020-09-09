a3ec8a4e3b8d24c867f8cfab2676b286d2c30fb1
Soumakakami <nkc.souma.kakami@gmail.com>
2020-09-09 10:46:33 +0900

    add:アニメーションとエフェクトを追加しました、またサンプルのFPS用コードなどをインストール

--- Action/Assets/Script/CSVTest.cs
+++ Action/Assets/Script/CSVTest.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 
 public class CSVTest : MonoBehaviour
 {
-    public Text text;
     void Start()
     {
         if (File.Exists("//172.18.133.1/GameLabo/student/たじまゼミ/各務/Test.csv"))
@@ -17,22 +16,18 @@ public class CSVTest : MonoBehaviour
         //text.text = "ここまできたよ１";
         StreamWriter sw = new StreamWriter("//172.18.133.1/GameLabo/student/たじまゼミ/各務/Test.csv", true, Encoding.GetEncoding("utf-8"));
         // ヘッダー出力
-        text.text = "ここまできたよ２";
         string[] s1 = { "プレイヤー名", "記録" };
         string s2 = string.Join(",", s1);
         sw.WriteLine(s2);
-        text.text = "ここまできたよ３";
         // データ出力
         for (int i = 0; i < 3; i++)
         {
-            text.text = "ここまできたよ４";
             string[] str = { "tatsu", "" + (i + 1) };
             string str2 = string.Join(",", str);
             sw.WriteLine(str2);
         }
         // StreamWriterを閉じる
         sw.Close();
-        text.text = "ここまできたよ５";
     }
 
     private void Update()
